37db642e03e10be2b8a93847492c5e3f62d0ba10
Harindra Wisnu Pradhana <harindrawisnu@gmail.com>
2020-01-21 15:52:45 +0700

    Update card parser
    refine the code, add feature to parse story point for cards using powerups

--- trelloparsecard.py
+++ trelloparsecard.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 import json
 import os
 import argparse
@@ -10,39 +10,57 @@ parser.add_help = True
 args = parser.parse_args()
 
 
-print("Reading Data...")
+print "Reading Data..."
 with open(os.path.abspath(args.input)) as f:
     data = json.load(f)
 
-print("Found {} cards in {} lists.".format(len(data['cards']), len(data['lists'])))
-print("Parsing...")
+print "Found {} cards in {} lists.".format(len(data['cards']), len(data['lists']))
+print "Parsing..."
 
 lists = {l['id']: l['name'] for l in data['lists']}
 users = {u['id']: u['fullName'] for u in data['members']}
 labels = {l['id']: l['name'] for l in data['labels']}
 
+"""
 parsed_cards = [{
     "name": c['name'],
-    # example of how to parse the name field...
-    #"study": c['name'].split()[1] if c['name'].split()[0]=='study' else '',
-    #"pkg": c['name'].split()[1] if c['name'].split()[0]=='pkg' else '',
     "list": lists[c['idList']],
-    "url": c['shortUrl'],
     "description": c['desc'],
     "members": [u for k, u in users.items() if k in c['idMembers']],
-    "labels": [l for k, l in labels.items() if k in c['idLabels']]
+    "labels": [l for k, l in labels.items() if k in c['idLabels']],
+    "plugin": c['pluginData']
 } for c in data['cards']]
+"""
+card = dict()
+cards = tuple()
+
+for c in data['cards'] :
+    card = {'id' : c['id']}
+    card.update({'list' : lists[c['idList']]})
+    card.update({'desc' : c['desc']})
+    card.update({'members': [u for k, u in users.items() if k in c['idMembers']]})
+    card.update({"labels": [l for k, l in labels.items() if k in c['idLabels']]})
+
+    # story point powerup data extract
+    for p in c['pluginData'] :
+        if 'value' in p :
+            value = json.loads(p['value'])
+            if 'points' in value :
+                card.update({'point' : value['points']})
+            else :
+                card.update({'value' : p['value']})
+        
+    cards += (card,)
 
 output = {
     "board_data": {
         "name": data['name'],
         "url": data['shortUrl']
     },
-    "cards": parsed_cards
+    "cards": cards
 }
 
 with open(os.path.abspath(args.output), 'w') as f:
     json.dump(output, f, indent=4)
 
-print("Output to {}!s".format(os.path.abspath(args.output)))
-print("Please visit https://json-csv.com/ to convert the output to CSV.")
+print "Output to {} !s".format(os.path.abspath(args.output))
